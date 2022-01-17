0a7e8e862bc9833f201fdbb52be648605132c9cc
Mcdonalds Trump <whdmschd2@gmail.com>
2022-01-17 21:41:30 +0900

    carousel resize 이벤트 3D tz 수정 전

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,9 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../css/carousel.css";
 import { cellsData } from "../components/cellsData.js";
 
 function Carousel () {
   const carousel = useRef();
+  const cells = useRef({});
+
+  const [windowSize, setWindowSize] = useState({width: window.innerWidth});
+  const [cellSz, setCellSz] = useState(1060);
+  const [tz, setTz] = useState(Math.round( ( cellSz / 2 ) /  Math.tan( Math.PI / cellsData.length ) ))
+
   let cellCount = cellsData.length;
   let selectedIndex = 0;
   let activeIndex = 0;
@@ -11,8 +17,9 @@ function Carousel () {
   let secondHidden = 5;
 
   let cellSize = 1060;
-  let numberOfCells = 9;
-  let tz = Math.round( ( cellSize / 2 ) /  Math.tan( Math.PI / numberOfCells ) );
+  let numberOfCells = cellsData.length;
+  // let tz = Math.round( ( cellSz / 2 ) /  Math.tan( Math.PI / numberOfCells ) );
+  console.log(tz)
 
   function rotateCarousel() {
     let angle = selectedIndex / cellCount * -360;
@@ -24,16 +31,19 @@ function Carousel () {
     carousel.current.children[0].classList.add("active");
     carousel.current.children[firstHidden].classList.add("hidden");
     carousel.current.children[secondHidden].classList.add("hidden");
+    carousel.current.children[0].children[1].classList.add("infoActive");
   }, [])
 
   function previousMove() {
     selectedIndex--;
 
     carousel.current.children[activeIndex].classList.remove("active");
+    carousel.current.children[activeIndex].children[1].classList.remove("infoActive");
 
     activeIndex === 0 ? activeIndex = 8 : activeIndex --
     hiddenActive(activeIndex);
     carousel.current.children[activeIndex].classList.add("active");
+    carousel.current.children[activeIndex].children[1].classList.add("infoActive");
 
     rotateCarousel();
   }
@@ -41,9 +51,11 @@ function Carousel () {
   function nextMove() {
     selectedIndex++;
     carousel.current.children[activeIndex].classList.remove("active");
+    carousel.current.children[activeIndex].children[1].classList.remove("infoActive");
     activeIndex === 8 ? activeIndex = 0 : activeIndex++
     hiddenActive(activeIndex);
     carousel.current.children[activeIndex].classList.add("active");
+    carousel.current.children[activeIndex].children[1].classList.add("infoActive");
 
     rotateCarousel();
   }
@@ -68,17 +80,48 @@ function Carousel () {
     carousel.current.children[secondHidden].classList.add("hidden");
   }
 
+  // window resize
+  // const [windowSize, setWindowSize] = useState({width: window.innerWidth});
+  // const [cellSz, setCellSz] = useState(1060);
+
+  function handleResize() {
+    // console.log(window.innerWidth - 97);
+    setWindowSize({width: window.innerWidth});
+    setCellSz(((window.innerWidth >= 1200) ? 1060 : (window.innerWidth - 97)));
+    setTz(Math.round( ( cellSz / 2 ) /  Math.tan( Math.PI / numberOfCells ) ));
+    console.log(cellSz);
+    
+    cellsData.map((data, i) => {
+      // console.log(cells.current[i].style.transform);
+      cells.current[i].style.transform = `rotateY(${i * 40}deg) translateZ(${tz}px)`
+    })
+    a();
+  }
+
+  function a () {
+    console.log(cellSz);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [])
+
   return(
     <div className="Carousel">
-      <div className="scene">
+      <div className="scene" style={{width: (windowSize.width >= 1200) ? "1060px" : `${(windowSize.width - 97)}px`}}>
         <div className="carousel" ref={carousel}>
           {cellsData.map((cell, index) => {
             return(
-              <div className="carousel__cell">
-                <img src={cell.img}></img>
+              <div className="carousel__cell" style={{width: (windowSize.width >= 1200) ? "1040px" : `${(windowSize.width - 117)}px`}} ref={ref => (cells.current[index] = ref)}>
+                <img src={cell.img} alt="img"></img>
                 <div className="carouselInfo">
                   <h2>{cell.title}</h2>
                   <h3>{cell.content}</h3>
+                  <hr></hr>
+                  <a><span>바로가기</span></a>
                 </div>
               </div>
             )
@@ -89,6 +132,8 @@ function Carousel () {
         <button className="previous-button" onClick={previousMove}>Previous</button>
         <button className="next-button" onClick={nextMove}>Next</button>
       </p>
+      <p>{cellSz}</p>
+      <p>tz:{tz}</p>
     </div>
     )
 }
